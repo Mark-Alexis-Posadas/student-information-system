38034ad7c03254e5af08cb38ab8dd7f139bff7e2
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-08-07 19:16:35 +0800

    implement changing icon based on state in nav menu:

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -1,18 +1,25 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faCaretDown, faMoon } from "@fortawesome/free-solid-svg-icons";
-import { useDispatch } from "react-redux";
+import {
+  faBars,
+  faCaretDown,
+  faMoon,
+  faXmark,
+} from "@fortawesome/free-solid-svg-icons";
+import { useDispatch, useSelector } from "react-redux";
 import { setIsToggle } from "../../reducers/toggleSlice";
+import { IsToggleType } from "../../types/states/isToggle";
 
 const Navbar: React.FC = () => {
   const dispatch = useDispatch();
+  const isToggle = useSelector((state: IsToggleType) => state.toggle.isToggle);
 
   return (
     <nav className="bg-white p-3 px-4 montserrat border-b border-slate-300">
       <ul className="flex items-center justify-between">
         <li className="flex items-center gap-4">
           <button onClick={() => dispatch(setIsToggle())}>
-            <FontAwesomeIcon icon={faBars} />
+            <FontAwesomeIcon icon={isToggle ? faXmark : faBars} />
           </button>
           <h3 className="font-bold">Student information system - admin</h3>
         </li>
