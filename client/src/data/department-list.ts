88e32c9e2e2519dea8be091e683470a2047ce725
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-08-10 16:56:37 +0800

    added department list data

---
+++ client/src/data/department-list.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const departmentListsData = [
+  {
+    id: uuidv4(),
+    text: "date created",
+  },
+  {
+    id: uuidv4(),
+    text: "name",
+  },
+  {
+    id: uuidv4(),
+    text: "description",
+  },
+  {
+    id: uuidv4(),
+    text: "status",
+  },
+
+  {
+    id: uuidv4(),
+    text: "action",
+  },
+];
